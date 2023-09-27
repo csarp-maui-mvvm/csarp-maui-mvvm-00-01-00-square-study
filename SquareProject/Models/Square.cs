dbe5b9dcadc67ebc53471888bf7c2dee191ec614
Csaba Gyuris <gyuris.csaba@vasvari.org>
2023-09-27 11:04:43 +0200

    Model réteg fejlesztése

---
+++ SquareProject/Models/Square.cs
@@ -0,0 +1,48 @@
+﻿using System.Reflection.Metadata;
+
+namespace SquareProject.Models
+{
+    public class Square
+    {
+        private double _side;
+        public double Side
+        {
+            get => _side;
+            set
+            {
+                if (value > 0)
+                {
+                    _side = value;
+                }
+            }
+        }
+
+        public Square(double side)
+        {
+            if (side > 0)
+                _side = side;
+            else
+                _side = 1;
+        }
+
+        public double Perimeter
+        {
+            get => 4 * _side;
+        }
+
+        public double Area
+        {
+            get => _side * _side;
+        }
+
+        public string AllProperty
+        {
+            get => $"{Side} méter a négyzet oldala.\nKerülete {Area} méter.\nTerülete {Area} négyzetméter.";
+        }
+
+        public override string ToString()
+        {
+            return AllProperty;
+        }
+    }
+}
